21733947bb05e32421d1eb657cb533cceff2983e
Sky Government <91508857+SkyGovernment@users.noreply.github.com>
2021-10-07 10:47:57 +0800

    Update EnumsSempoi.sol

--- contracts/EnumsSempoi.sol
+++ contracts/EnumsSempoi.sol
@@ -1,5 +1,26 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
-pragma ^0.8.0;
+pragma solidity ^0.8.0;
 
-contract EnumsSempoi{}
+contract EnumsSempoi{
+    uint8 rarity = 0;
+    
+    enum Rarity{
+        original, // 0
+        rare, // 1
+        super_rare // 2
+    }
+    
+    Rarity public rarity;
+    
+    // constructor get call only once during deployment
+    // constructor diseru sekali sahaja ketika deployment
+    constructor() {
+        rarity = Rarity.rare;
+    }
+    
+    function makeSuperRare() public {
+        rarity = Rarity.super_rare;
+    }
+    
+}
