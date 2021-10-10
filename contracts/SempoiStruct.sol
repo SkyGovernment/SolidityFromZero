41f368931f42b27d1c5e14e0b961d77f765b1680
Sky Government <91508857+SkyGovernment@users.noreply.github.com>
2021-10-10 14:30:30 +0800

    Update SempoiStruct.sol

--- contracts/SempoiStruct.sol
+++ contracts/SempoiStruct.sol
@@ -14,11 +14,12 @@ contract SempoiStruct{
         // NFT memory newNFT;
         // newNFT.name = _name;
         // newNFT.dna = _dna;
-        NFT memory newNFT = NFT(_name, _dna);
+        NFT memory newNFT = NFT(_name, _dna); // This is the same as 3 lines of code above
         nftList.push(newNFT);
     }
     
     function addNFTS(NFT[] calldata _nfts) public {
+        //calldata only for Array
         nftList = _nfts;
     }
     
@@ -30,7 +31,7 @@ contract SempoiStruct{
     function updateNFTMemory(uint256 _index, string memory _name) public {
         NFT memory nftToBeUpdated = nftList[_index];
         nftToBeUpdated.name = _name;
-        nftList[_index] = nftToBeUpdated;
+        nftList[_index] = nftToBeUpdated; // need this or error
     }
     
     function getNftName(uint256 _index) public view returns(string memory){
