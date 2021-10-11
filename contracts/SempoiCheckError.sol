33f3a4d723e476721d72df3aa60541fa8e4dca68
Sky Government <91508857+SkyGovernment@users.noreply.github.com>
2021-10-11 22:48:42 +0800

    Update SempoiCheckError.sol

--- contracts/SempoiCheckError.sol
+++ contracts/SempoiCheckError.sol
@@ -3,4 +3,27 @@
 pragma solidity ^0.8.0;
 
 contract SempoiCheckError{
+  uint256 public maxAmount = 100;
+    
+    function updateMax() public {
+        maxAmount = 50;
+    }
+    
+   function myFunc(uint256 _x, uint256 _y) public view returns (uint256 xy){
+       require(_x < _y, "X is bigger that Y");
+       checkMax();
+       return _x + _y;
+   }
+   
+   function myPureRevertFunc(uint256 _x, uint256 _y) public pure returns (uint256 xy){
+        //  logic
+        if(_x == 10) {
+            revert("X is 10");
+        }
+       return _x + _y;
+   }
+   
+   function checkMax() internal view {
+       assert(maxAmount == 100);
+   }
 }
